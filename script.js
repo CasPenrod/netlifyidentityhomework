a9f3abd7e2a1a02dee4ca9cb4422f7a39ed6e5ec
Cassandra-Penrod <cpenrod@proforma.com>
2022-07-15 11:42:11 -0400

    jh k

--- script.js
+++ script.js
@@ -3,45 +3,29 @@ let app = angular.module("portfolioSignIn", ["ngRoute"]);
 app.config(function ($routeProvider) {
   $routeProvider
     .when("/", {
-      templateUrl: "login-signup.html.html",
+      templateUrl: "login-signup.html",
     })
-    .when("/", {
-      templateUrl: "index.html",
+    .when("login-signup", {
+      templateUrl: "login-signup.html",
     })
     .when("/portfolio", {
       templateUrl: "portfolio.html",
     });
 });
 
-// Open the modal
-netlifyIdentity.open();
-
-// Get the current user:
-// Available after on('init') is invoked
-const user = netlifyIdentity.currentUser();
-
-// Bind to events
-netlifyIdentity.on("init", (user) => console.log("init", user));
-netlifyIdentity.on("login", (user) => console.log("login", user));
-netlifyIdentity.on("logout", () => console.log("Logged out"));
-netlifyIdentity.on("error", (err) => console.error("Error", err));
-netlifyIdentity.on("open", () => console.log("Widget opened"));
-netlifyIdentity.on("close", () => console.log("Widget closed"));
-
-// Unbind from events
-netlifyIdentity.off("login"); // to unbind all registered handlers
-netlifyIdentity.off("login", handler); // to unbind a single handler
-
-// Close the modal
-netlifyIdentity.close();
-
-// Log out the user
-netlifyIdentity.logout();
-
-// Refresh the user's JWT
-// Call in on('login') handler to ensure token refreshed after it expires (1hr)
-// Note: this method returns a promise.
-netlifyIdentity.refresh().then((jwt) => console.log(jwt));
-
-// Change language
-netlifyIdentity.setLocale("en");
+app.controller("portfolio", function ($scope, $http) {
+  let initUser = netlifyIdentity.currentUser();
+  netlifyIdentity.on("init", () => {
+    initUser = netlifyIdentity.currentUser();
+  });
+  netlifyIdentity.on("login", () => {
+    if (initUser == null) {
+      window.location.replace("#!portfolio");
+    }
+    netlifyIdentity.close();
+  });
+  netlifyIdentity.on("logout", () => {
+    netlifyIdentity.close();
+    window.location.replace("/");
+  });
+});
